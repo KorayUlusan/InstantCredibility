764b68fa7d33eb3c8c546f5d31874ef3d422ffa3
Koray Ulusan <korayulusan2@gmail.com>
2022-06-02 17:35:07 +0200

    first draft

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
-const images = [
-  "https://upload.wikimedia.org/wikipedia/commons/5/5e/Abrid_n_temzi.jpg",
-  "https://upload.wikimedia.org/wikipedia/commons/d/d1/Above_Sprouts_Gorcery_Store_Tonight_Fort_Collins_Colorado_O_%28229896797%29.jpeg",
-  "https://upload.wikimedia.org/wikipedia/commons/b/bd/95865267_Jpg_%28165471921%29.jpeg",
-  "https://upload.wikimedia.org/wikipedia/commons/5/52/500px_photo_%2869274049%29.jpeg",
-  "https://upload.wikimedia.org/wikipedia/commons/b/b6/500px_photo_%2869274077%29.jpeg",
-  "https://upload.wikimedia.org/wikipedia/commons/e/ed/500px_photo_%2869846341%29.jpeg",
-  "https://upload.wikimedia.org/wikipedia/commons/f/f1/4197590619_Jpg_%28165456841%29.jpeg",
-  "https://upload.wikimedia.org/wikipedia/commons/6/60/15038704942_Jpg_%28165453195%29.jpeg",
-  "https://upload.wikimedia.org/wikipedia/commons/b/b3/15361545496_Jpg_%28165453177%29.jpeg",
-  "https://upload.wikimedia.org/wikipedia/commons/5/5b/080307-188-Kekeko.jpg",
-  "https://upload.wikimedia.org/wikipedia/commons/8/8d/-newbeginning_-sunraising.jpg",
-  "https://upload.wikimedia.org/wikipedia/commons/9/9c/-nature.jpg",
-  "https://upload.wikimedia.org/wikipedia/commons/4/4c/%22%D0%9E%D1%81%D1%82%D0%B0%D0%B2%D1%88%D0%B8%D0%B9%D1%81%D1%8F%22.jpg",
-];
+"use strict";
 
-function get_random(list) {
-  return list[Math.floor(Math.random() * list.length)];
+const params = new URLSearchParams(document.location.search);
+
+var args = {
+  quote: params.get("q"),
+  author: params.get("a"),
+  year: params.get("y"),
+};
+if (args.quote) {
+  document.getElementById("quote").innerHTML = args.quote;
 }
-
-var bg = document.getElementById("bg");
-bg.src = get_random(images);
-bg.style.maxWidth = "90vw"
-bg.style.maxHeight = "80vh"
+if (args.author && args.year) {
+  document.getElementById("origin").innerHTML = `- ${args.author}, ${args.year}`;
+} else if (args.author) {
+  document.getElementById("origin").innerHTML = `- ${args.author}`;
+}
