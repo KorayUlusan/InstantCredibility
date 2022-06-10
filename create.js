6dfbb896d9910609842d3af06720830dd3821dbf
Koray Ulusan <korayulusan2@gmail.com>
2022-06-10 19:52:01 +0200

    preview empty quotes

--- create.js
+++ create.js
@@ -10,7 +10,8 @@ const quoteUrl = location.href.split(htmlName)[0] + "quote.html";
 function createUrl() {
   var quoteInput = document.getElementById("quote");
   if (!quoteInput.value) {
-    return quoteUrl;
+    // dummy: don't create a quote
+    return quoteUrl + "?q=%20";
   }
   var authorInput = document.getElementById("author");
   var yearInput = document.getElementById("year");
@@ -29,7 +30,7 @@ function displayQuote() {
 }
 
 var timeout = null;
-document.addEventListener("keyup", () => {
+document.getElementById("generatorbox").addEventListener("keyup", () => {
   // using timeouts because otherwise when the user types
   // too fast the iframe takes some time to load,
   // which results in not displaying preview.
